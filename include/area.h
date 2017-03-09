57372be94abf95f124964ca4d691000269e097e5
mattheus10 <augusto01a@gmail.com>
2017-03-09 14:25:30 -0300

    teste de adesão

---
+++ include/area.h
@@ -0,0 +1,6 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+float area_Triangulo(float altura, float base);
+
+#endif // HEADER_H
---
+++ src/area.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "area.h"
+
+using namespace std;
+
+float area_Triangulo(float altura, float base)
+{
+	float result;
+
+	result = (base * altura)/2;
+
+	return result;
+}
