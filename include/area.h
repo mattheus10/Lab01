32b0f574d8e7db4b84b2f35900d1db8b197f764b
mattheus10 <augusto01a@gmail.com>
2017-03-12 15:32:26 -0300

    Adicionado a main

--- include/area.h
+++ include/area.h
@@ -10,4 +10,4 @@ float area_Cubo(float area_daFace);
 float area_Paralelepipedo(float area_daBase, float area_Lateral1, float area_Lateral2);
 float area_Esfera(float raio);
 
-#endif // HEADER_H#endif // AREA_H
+#endif // AREA_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cstdlib>
-#include "include\area.h"
+#include "include\calcarea.h"
+#include "include\calcperimetro.h"
+#include "include\calcvolume.h"
 
 using namespace std;
 
@@ -8,11 +10,53 @@ using namespace std;
 
 int main()
 {
-	float x, y;
+	int figura = 10;	
 
-	std::cout << "Digita" << "\n";
-	cin >> x; 
-	cin >> y;
-	area_Triangulo(x,y);
+	while(figura != 0){
+		
+		std::cout << "Calculadora de Geometria Plana e Espacial \n(1) Triangulo equilatero\n(2) Retangulo\n(3) Quadrado\n(4) Circulo\n(5) Piramide com base quadrangular\n(6) Cubo\n(7) Paralelepipedo\n(8) Esfera\n(0) Sair Digite a sua opcao: "<< endl;
+		std::cin >> figura;
+		
+		//Solução pro caso de entradas fora do intervalo.
+		while(figura < 0 || figura > 8){
+			std::cout << "Numero inválido tente novamente:" << endl;
+			std::cout << "Calculadora de Geometria Plana e Espacial \n(1) Triangulo equilatero\n(2) Retangulo\n(3) Quadrado\n(4) Circulo\n(5) Piramide com base quadrangular\n(6) Cubo\n(7) Paralelepipedo\n(8) Esfera\n(0) Sair Digite a sua opcao: "<< endl;
+			std::cin >> figura;
+		}
+		if(figura == 1) {
+			calcarea(figura);
+			calcperimetro(figura);
+		}
+		else if(figura == 2) {
+			calcarea(figura);
+			calcperimetro(figura);
+		}
+		else if(figura == 3) {
+			calcarea(figura);
+			calcperimetro(figura);
+		}
+		else if(figura == 4) {
+			calcarea(figura);
+			calcperimetro(figura);
+		}
+		else if(figura == 5) {
+			calcarea(figura);
+			calcvolume(figura);
+		}
+		else if(figura == 6) {
+			calcarea(figura);
+			calcvolume(figura);
+		}
+		else if(figura == 7) {
+			calcarea(figura);
+			calcvolume(figura);
+		}
+		else if(figura == 8) {
+			calcarea(figura);
+			calcvolume(figura);
+		}
+	}
+
+	cout << "===FIM DE EXECUÇÃO===" << endl;
 	//return 0;
 }
